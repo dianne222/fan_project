a7efe0e32bbfe6757d7156ad81156fc6b7339480
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 03:04:35 +0800

    added a private string data field named color

--- fan.py
+++ fan.py
@@ -3,7 +3,8 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    def __init__(self, speed, on=False, radius):
+    def __init__(self, speed, on=False, radius, color):
         self.__speed = speed
         self.__on = on
         self.__radius = radius
+        self.__color = color
