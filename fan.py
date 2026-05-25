d48b1e14172e99fa4de3796b6f9f72e044348af9
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 03:10:52 +0800

    set the default values for data fields

--- fan.py
+++ fan.py
@@ -3,11 +3,11 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    def __init__(self, speed, on=False, radius, color):
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
         self.__speed = speed
-        self.__on = on
         self.__radius = radius
         self.__color = color
+        self.__on = on
 
     # Getters
     def get_speed(self):
