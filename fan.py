70bc2803c010d2cf8c3d13d3caccc2267485c1f1
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 03:29:28 +0800

    changed some code arrangements

--- fan.py
+++ fan.py
@@ -13,24 +13,24 @@ class Fan:
     def get_speed(self):
         return self.__speed
 
-    def get_on(self):
-        return self.__on
-
     def get_radius(self):
         return self.__radius
 
     def get_color(self):
         return self.__color
 
+    def get_on(self):
+        return self.__on
+
     # Setters
     def set_speed(self, speed):
         self.__speed = speed
-
-    def set_on(self, on):
-        self.__on = on
 
     def set_radius(self, radius):
         self.__radius = radius
 
     def set_color(self, color):
         self.__color = color
+
+    def set_on(self, on):
+        self.__on = on
